8f7360cb5c492307fb2382da6003d1d56ce7b0c8
JorgeGuty <49891177+JorgeGuty@users.noreply.github.com>
2019-10-17 22:49:03 -0600

    Creadas Clase Nodo y Clase Grafo
    Le implementé los metodos básicos a la clase nodo, no sé si le falta alguno, ahi te fijas

---
+++ src/Grafo/Graph.java
@@ -0,0 +1,11 @@
+package Grafo;
+
+//This design is implemented with an adyacence list.
+
+public class Graph<T> {
+
+    Node<T> start;
+
+
+
+}
---
+++ src/Grafo/Node.java
@@ -0,0 +1,52 @@
+package Grafo;
+
+import java.util.ArrayList;
+
+//This design is implemented with an adyacence list.
+
+public class Node<T> {
+
+    private ArrayList<Node<T>> arches;
+    private Node<T> nextNode;
+    private String searchKey;
+    private T element;
+
+    public Node(String pSearchKey, T pElement) {
+        searchKey = pSearchKey;
+        element = pElement;
+        arches = new ArrayList<>();
+        nextNode = null;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public void addArch(Node<T> pNewArch){
+        arches.add(pNewArch);
+    }
+
+    public void deleteArch(Node<T> pToEliminate){
+        arches.remove(pToEliminate);
+    }
+
+    public ArrayList<Node<T>> getArches() {
+        return arches;
+    }
+
+    public String getSearchKey() {
+        return searchKey;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public void setElement(T element) {
+        this.element = element;
+    }
+}
