c807eb0227996f0e3a0daf834f0468ac24d6b11d
JorgeGuty <49891177+JorgeGuty@users.noreply.github.com>
2019-10-19 15:18:17 -0600

    Update MainWindow.java

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -53,7 +53,7 @@ public class MainWindow extends JFrame implements IConstants{
 		buttonSimular.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
+				myMapPanel.setInitRecorido(true);
 			}
 		});
 	}
