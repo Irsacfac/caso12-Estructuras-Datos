51a8f0478ca4971a11fa3e99ff01fa5678fb35e9
Isaac Ortega Arguedas <irsacfac@gmail.com>
2019-10-18 22:59:02 -0600

    algo

--- src/otros/IConstants.java
+++ src/otros/IConstants.java
@@ -1,5 +1,12 @@
 package otros;
 
 public interface IConstants {
-
+	public static int PANTALLA_ANCHURA = 1800;
+	public static int PANTALLA_ALTURA = 1000;
+	public static int DEFAULT_BUTTON_ANCHURA = 80;
+	public static int DEFAULT_BUTTON_ALTURA = 40;
+	public static int RANGO_BUSQUEDA = 30;
+	public static int PANEL_ANCHURA = 200;
+	public static int RADIO_CIRCULO = 30;
+	public static int MOUSE_ADJUSTMENT = (int)RADIO_CIRCULO/2;
 }
